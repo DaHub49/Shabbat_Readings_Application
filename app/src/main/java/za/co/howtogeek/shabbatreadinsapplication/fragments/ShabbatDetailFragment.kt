1ad556714b3718f5346547689facca1ca31a593d
DaHub49 <dyls.martin.49@gmail.com>
2024-10-27 07:15:56 +0200

    27/10/2024 @07:15: Updated adapter project to load the correct week in ShabbatDetailFragment after selecting it in the recyclerview. The BibleTranslationFragment opens correctly, but I am yet to program and test the functionality.
    Still to do: check the logic of the translationFragment, link up all the textviews on the home page, and add the additionalreadings fragment.

--- app/src/main/java/za/co/howtogeek/shabbatreadinsapplication/fragments/ShabbatDetailFragment.kt
+++ app/src/main/java/za/co/howtogeek/shabbatreadinsapplication/fragments/ShabbatDetailFragment.kt
@@ -7,7 +7,6 @@ import android.content.SharedPreferences
 import android.content.res.AssetManager
 import android.net.Uri
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
