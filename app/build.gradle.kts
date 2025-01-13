2709b9287691abc0e52a26ff4834c34a4b00a52f
DaHub49 <dyls.martin.49@gmail.com>
2025-01-13 14:48:25 +0200

    13/01/25 @14:48: -> Updated version to 46

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "za.co.howtogeek.shabbatreadingsapplication"
         minSdk = 22
         targetSdk = 35
-        versionCode = 45
-        versionName = "Update of Berasheet 5785"
+        versionCode = 46
+        versionName = "Shemot 5785"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
